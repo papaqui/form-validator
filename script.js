3402e6ddb590e577b5bf74abcbd04eeffa5071f0
Fernando <hola@rivaslevi.mx>
2021-01-31 07:51:04 -0600

    initial commit

---
+++ script.js
@@ -0,0 +1,88 @@
+'use strict';
+
+// const username = document.getElementById('username').value;
+
+// document.getElementById('submitBtn').addEventListener('click', function () {
+//   if (username == '') {
+//     console.log('empty');
+//     document.getElementById('errorUsername').classList.add('visible');
+//   }
+// });
+
+// Collect data from DOM
+const form = document.getElementById('form');
+const username = document.getElementById('username');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const password2 = document.getElementById('password2');
+
+// Show input error message
+function showError(input, message) {
+  const formControl = input.parentElement;
+  formControl.className = 'form-control error';
+  const small = formControl.querySelector('small');
+  small.innerText = message;
+}
+
+// Show success outline
+function showSuccess(input) {
+  const formControl = input.parentElement;
+  formControl.className = 'form-control success';
+}
+
+// Check email is valid
+function isValidEmail(email) {
+  const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+}
+
+// Check required fields
+function checkRequired(inputArr) {
+  inputArr.forEach(function (input) {
+    if (input.value.trim() === '') {
+      showError(input, `${getFieldName(input)} is required`);
+    } else {
+      showSuccess(input);
+    }
+  });
+}
+
+// Get fieldname
+function getFieldName(input) {
+  return input.id.charAt(0).toUpperCase() + input.id.slice(1);
+}
+
+// Event listeners
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
+
+  // Sloppy version
+  // if (username.value === '') {
+  //   showError(username, 'Username is required');
+  // } else {
+  //   showSuccess(username);
+  // }
+
+  // if (email.value === '') {
+  //   showError(email, 'Email is required');
+  // } else if (!isValidEmail(email.value)) {
+  //   showError(email, 'Email is not valid');
+  // } else {
+  //   showSuccess(email);
+  // }
+
+  // if (password.value === '') {
+  //   showError(password, 'Password is required');
+  // } else {
+  //   showSuccess(password);
+  // }
+  // if (password2.value === '') {
+  //   showError(password2, 'Confirm password is required');
+  // } else if (password2 !== password) {
+  //   showError(password2, "Passwords doesn't match");
+  // } else {
+  //   showSuccess(password2);
+  // }
+
+  checkRequired([username, email, password, password2]);
+});
